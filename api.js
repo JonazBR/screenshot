7ef0fe700e1eb9e44edd7c580d5eb20830c1d77f
jonaz_dev <56744848+JonazBR@users.noreply.github.com>
2024-03-02 21:22:20 -0400

    Update api.js

--- api.js
+++ api.js
@@ -6,7 +6,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 async function takeScreenshot(url) {
-  const browser = await playwright.launch({
+  const browser = await playwright.chromium.launch({
     args: chromium.args,
     executablePath: await chromium.executablePath,
     headless: chromium.headless
